c56794e33e7349890218ecde655c5ded2b3e17e4
Peter Cashel <peter@CASHELHOME-LINUXSERVER.cashelhome.local>
2026-02-25 05:57:34 +1000

    Reorganise and add quick dirty instructions.

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '4c13a2c2'
+version = '0.1.0+dev.4c13a2c2'
+linkified_version = '0.1.0+dev.[4c13a2c2](https://github.com/maunium/stickerpicker/commit/4c13a2c254d68980bcf43f132e0b78e3c08ed3b1)'
