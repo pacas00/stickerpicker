e8a5f30d142b27ddb7891c352fed857afff0c314
Peter Cashel <pacas00@gmail.com>
2026-02-28 22:22:18 +1000

    Repacked with 1 animated one, Pushing

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '4c13a2c2'
-version = '0.1.0+dev.4c13a2c2'
-linkified_version = '0.1.0+dev.[4c13a2c2](https://github.com/maunium/stickerpicker/commit/4c13a2c254d68980bcf43f132e0b78e3c08ed3b1)'
+git_revision = '05aaa5df'
+version = '0.1.0+dev.05aaa5df'
+linkified_version = '0.1.0+dev.[05aaa5df](https://github.com/maunium/stickerpicker/commit/05aaa5df036fb8c1c6e8fe67d7c2f03ee351c455)'
